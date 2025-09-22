6fee4a73ee8b5e6695db114d449d134c5d4d8d8b
lil Eliel <eliechamakone@gmail.com>
2025-09-21 20:45:28 -0400

    J'ai changé la stucturation du fichier

--- src/main/java/com/mycompany/tickets/Admin.java
+++ src/main/java/com/mycompany/tickets/Admin.java
@@ -2,10 +2,50 @@ package com.mycompany.tickets;
 
 import java.util.List;
 
-public interface Admin {
-    void assignTicket(Ticket ticket, User user);
-     void closeTicket(Ticket ticket);
-     List<Ticket> viewAllTickets();
+public class Admin {
+    private int adminID;
+    private String name;
+    private String email;
+public Admin() {
 
+}
+    public Admin(int adminID, String name, String email) {
+        this.adminID = adminID;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getAdminID() {
+        return adminID;
+    }
+
+    public void setAdminID(int adminID) {
+        this.adminID = adminID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    void assignTicket(Ticket ticket, User user){
+
+    }
+    void closeTicket(Ticket ticket){
+
+    }
+    List<Ticket> viewAllTickets() {
+
+    };
 
 }
--- src/main/java/com/mycompany/tickets/User.java
+++ src/main/java/com/mycompany/tickets/User.java
@@ -1,9 +1,56 @@
 package com.mycompany.tickets;
 
-public interface User {
+public class User {
+    private int userID;
+    private String name;
+    private String email;
 
-    void createTicket(Ticket ticket);
-    void viewTicket(Ticket ticket);
-    void updateTicket(Ticket ticket);
+    public User(){
+
+    }
+
+    public User(int userID, String name, String email, String role) {
+        this.userID = userID;
+        this.name = name;
+        this.email = email;
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    private String role;
+    void createTicket(Ticket ticket){}
+    void viewTicket(Ticket ticket){}
+    void updateTicket(Ticket ticket){}
 
 }
